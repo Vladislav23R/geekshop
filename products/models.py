70df5f9cf5628f6146be01f179d5d047ff640530
Vladislav Raev <streletz00@gmail.com>
2021-06-02 18:49:23 +0300

    added superuser and register models

--- products/models.py
+++ products/models.py
@@ -9,10 +9,15 @@ class ProductCategory(models.Model):
         return self.name
 
 
-class Products(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=128)
     image = models.ImageField(upload_to='products_image', blank=True)
     description = models.CharField(max_length=128, blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} | {self.category.name}"
+
+
