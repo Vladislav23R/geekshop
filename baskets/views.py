a83caa2c3428094e13d8221ec6d75315b86bc85a
Vladislav Raev <streletz00@gmail.com>
2021-06-10 21:42:54 +0300

    added basket_add function to controller

--- baskets/views.py
+++ baskets/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
 
-# Create your views here.
+
+from products.models import Product
+from baskets.models import Baskets
+
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Baskets.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Baskets.objects.create(user=request.user, product=product, quantity=1)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
