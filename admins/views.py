60e0ea33ec60f069e69c3ba947d200662970f086
Vladislav Raev <streletz00@gmail.com>
2021-06-16 22:04:50 +0300

    added create user for admin

--- admins/views.py
+++ admins/views.py
@@ -1,18 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
+from django.urls import reverse
 
 from users.models import User
+from admins.forms import UserAdminRegisterForm
+
 
 def index(request):
     return render(request, 'admins/admin.html')
 
 
 def admin_users(request):
-    context = {'users': User.objects.all()}
+    context = {'title': 'GeekShop - Админ | Пользователи', 'users': User.objects.all()}
     return render(request, 'admins/admin-users-read.html', context)
 
 
 def admin_users_create(request):
-    return render(request, 'admins/admin-users-create.html')
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegisterForm()
+    context = {'title': 'GeekShop - Админ | Регистрация', 'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
 
 
 def admin_users_update(request, id):
