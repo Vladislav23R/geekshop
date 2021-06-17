ce0efe17cda949af277526f198f735fbef29e1d0
Vladislav Raev <streletz00@gmail.com>
2021-06-17 22:46:25 +0300

    update files

--- admins/views.py
+++ admins/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import user_passes_test
 
